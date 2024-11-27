df957c6bff02aafd791cebc560f9e289501d5c49
amida <cain.wells@mail.ru>
2024-11-27 21:16:37 +0300

    Inital commit

---
+++ data-fetcher/src/test/java/org/amida/data_fetcher/DataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amida.data_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
