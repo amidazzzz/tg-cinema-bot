6f5de976cf39ee1c7d8fe0586dfa1fd76ecf8fb8
amida <cain.wells@mail.ru>
2024-12-01 17:05:29 +0300

    feat(config): add WebClientConfig for centralized WebClient configuration

--- data-fetcher/src/main/java/org/amida/data_fetcher/config/WebClientConfig.java
+++ data-fetcher/src/main/java/org/amida/data_fetcher/config/WebClientConfig.java
@@ -1,4 +1,20 @@
 package org.amida.data_fetcher.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Configuration
 public class WebClientConfig {
+
+    @Value("${API.URL}")
+    private String apiUrl;
+
+    @Bean
+    public WebClient webClientBuilder(WebClient.Builder builder) {
+        return builder.baseUrl(apiUrl).build();
+    }
 }
