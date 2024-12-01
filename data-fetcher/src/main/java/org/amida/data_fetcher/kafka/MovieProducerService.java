89990d84ada3190967bca18e8b9b4b0b42244bf5
amida <cain.wells@mail.ru>
2024-12-01 20:54:47 +0300

    feat(kafka): add MovieProducerService to send Movie data to Kafka topics

--- data-fetcher/src/main/java/org/amida/data_fetcher/kafka/MovieProducerService.java
+++ data-fetcher/src/main/java/org/amida/data_fetcher/kafka/MovieProducerService.java
@@ -1,4 +1,21 @@
 package org.amida.data_fetcher.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.amida.data_fetcher.model.Movie;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MovieProducerService {
+
+    private final KafkaTemplate<String, Movie> kafkaTemplate;
+
+
+    public void send(String topic, String key, Movie movie) {
+        kafkaTemplate.send(topic, key, movie);
+        log.info("Sent movie data to topic {} with key {}", topic, key);
+    }
 }
