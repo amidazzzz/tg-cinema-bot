fae0ad29b40ffcac59b6a67271bd7c164b2ec466
amida <cain.wells@mail.ru>
2024-12-01 20:53:09 +0300

    chore(model): remove unnecessary imports in Movie class

--- data-fetcher/src/main/java/org/amida/data_fetcher/model/Movie.java
+++ data-fetcher/src/main/java/org/amida/data_fetcher/model/Movie.java
@@ -1,4 +1,15 @@
 package org.amida.data_fetcher.model;
 
-public record Movie(String title, String year, String genre, String director) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Movie(
+        @JsonProperty("Title")
+        String title,
+        @JsonProperty("Year")
+        String year,
+        @JsonProperty("Genre")
+        String genre,
+        @JsonProperty("Director")
+        String director
+) {
 }
