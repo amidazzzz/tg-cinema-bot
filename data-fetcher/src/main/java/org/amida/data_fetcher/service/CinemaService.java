6e5a1a1fccc7847e9af1a32c1be1ce6045aa97e0
amida <cain.wells@mail.ru>
2024-12-01 16:54:11 +0300

    feat(data-fetcher): add CinemaService to fetch movie data from OMDb API

--- data-fetcher/src/main/java/org/amida/data_fetcher/service/CinemaService.java
+++ data-fetcher/src/main/java/org/amida/data_fetcher/service/CinemaService.java
@@ -1,4 +1,28 @@
 package org.amida.data_fetcher.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
 public class CinemaService {
+
+    @Value("${API.KEY}")
+    private final String apiKey;
+
+    private final WebClient webClient;
+
+    public String fetchMovieData(String title){
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("t", title)
+                        .queryParam("apiKey", apiKey)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
