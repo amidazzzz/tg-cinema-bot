1ea4f97dc768aac9a7c9aa310b10de7561c7fd2c
amida <cain.wells@mail.ru>
2024-12-11 11:23:13 +0300

    (tg-bot): Initial commit

---
+++ data-fetcher/src/test/java/org/amida/data_fetcher/kafka/MovieConsumerServiceTest.java
@@ -0,0 +1,7 @@
+package org.amida.data_fetcher.kafka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieConsumerServiceTest {
+
+}
---
+++ data-fetcher/src/test/java/org/amida/data_fetcher/service/CinemaServiceTest.java
@@ -0,0 +1,12 @@
+package org.amida.data_fetcher.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CinemaServiceTest {
+
+    @Test
+    void fetchMovieData() {
+    }
+}
