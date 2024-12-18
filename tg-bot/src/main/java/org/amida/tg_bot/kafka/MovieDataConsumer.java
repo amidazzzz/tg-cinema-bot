77c9a4f7a77d6dad2752b92e31a644f4b6a2ffe2
amida <cain.wells@mail.ru>
2024-12-18 21:47:28 +0300

    feat(tg-bot): improve MovieDataConsumer and MovieRequestProducer - Refactor message handling in MovieDataConsumer. - Optimize message sending logic in MovieRequestProducer.

--- tg-bot/src/main/java/org/amida/tg_bot/kafka/MovieDataConsumer.java
+++ tg-bot/src/main/java/org/amida/tg_bot/kafka/MovieDataConsumer.java
@@ -1,4 +1,22 @@
 package org.amida.tg_bot.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.amida.tg_bot.model.Movie;
+import org.amida.tg_bot.service.MovieInfoBotService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MovieDataConsumer {
+
+    private final MovieInfoBotService movieInfoBotService;
+
+    @KafkaListener(topics = "movie-data-responses", groupId = "telegram-bot-group")
+    public void consumeMovieData(Movie movie){
+        log.info("Received movie data from Kafka {}", movie);
+        movieInfoBotService.handleMovieData(movie);
+    }
 }
--- tg-bot/src/main/java/org/amida/tg_bot/kafka/MovieRequestProducer.java
+++ tg-bot/src/main/java/org/amida/tg_bot/kafka/MovieRequestProducer.java
@@ -1,4 +1,19 @@
 package org.amida.tg_bot.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MovieRequestProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMovieRequest(String movieTitle) {
+        kafkaTemplate.send("fetch-movie-requests", movieTitle);
+        log.info("Sent movie request to Kafka: {}", movieTitle);
+    }
 }
