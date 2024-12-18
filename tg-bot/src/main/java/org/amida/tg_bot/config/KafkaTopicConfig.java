9e7e5bae0d775f05c151f217725b95d5d44979ce
amida <cain.wells@mail.ru>
2024-12-18 21:46:25 +0300

    feat(tg-bot): update Kafka consumer, producer, and topic configurations - Improve KafkaConsumerConfig and KafkaProducerConfig for better handling of messages. - Update KafkaTopicConfig with refined topic settings.

--- tg-bot/src/main/java/org/amida/tg_bot/config/KafkaTopicConfig.java
+++ tg-bot/src/main/java/org/amida/tg_bot/config/KafkaTopicConfig.java
@@ -1,4 +1,14 @@
 package org.amida.tg_bot.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic movieRequestTopic() {
+        return new NewTopic("fetch-movie-requests", 1, (short) 1);
+    }
 }
