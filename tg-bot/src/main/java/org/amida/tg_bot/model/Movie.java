cf9cd77075995ba29fce9a28b631964df71ce9c9
amida <cain.wells@mail.ru>
2024-12-18 21:47:55 +0300

    feat(tg-bot): add Movie model class

--- tg-bot/src/main/java/org/amida/tg_bot/model/Movie.java
+++ tg-bot/src/main/java/org/amida/tg_bot/model/Movie.java
@@ -1,4 +1,17 @@
 package org.amida.tg_bot.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Movie {
+    private String title;
+    private String year;
+    private String genre;
+    private String director;
+
 }
